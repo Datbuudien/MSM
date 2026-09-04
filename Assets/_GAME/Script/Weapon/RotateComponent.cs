a0010e3e191f6111ddc1029b99ab24d107dae236
Datbuudien <nguyenducdatva@gmail.com>
2026-09-04 17:33:27 +0700

    Fix rotate weapon

--- Assets/_GAME/Script/Weapon/RotateComponent.cs
+++ Assets/_GAME/Script/Weapon/RotateComponent.cs
@@ -4,11 +4,14 @@ public class RotateComponent : MonoBehaviour
 {
     [SerializeField] private Transform tf;
     [SerializeField] private float speedRotation = 1080f;
-    float z =0f;
+    private float z;
+    void OnEnable()
+    {
+        z =0f;
+    }
     void Update()
     {
-        z = (z+speedRotation*Time.deltaTime)%360f;
-        Vector3 tmp = tf.localEulerAngles;
-        tf.localRotation= Quaternion.Euler(tmp.x,tmp.y,z);
+        z = (z+speedRotation*Time.deltaTime)%360f;     
+        tf.localRotation= Quaternion.Euler(270,0,z);
     }
 }
